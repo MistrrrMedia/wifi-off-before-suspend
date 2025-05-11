5fd2f1578fd0caf5eb0d9f72d5087cff9b07aace
MistrrrMedia <65423117+MistrrrMedia@users.noreply.github.com>
2025-05-11 02:17:45 -0500

    Update wifi-off-before-suspend.sh
    fixed logger spelling

--- wifi-off-before-suspend.sh
+++ wifi-off-before-suspend.sh
@@ -2,10 +2,10 @@
 case $1/$2 in
   pre/*)
     nmcli radio wifi off
-    logger "[wiif-off-before-suspend] Wifi Disabled"
+    logger "[wifi-off-before-suspend] Wifi Disabled"
     ;;
   post/*)
     nmcli radio wifi on
-    logger "[wiif-off-before-suspend] Wifi Reenabled"
+    logger "[wifi-off-before-suspend] Wifi Reenabled"
     ;;
 esac
